96f3b64db1873915760231e3419a13eecadbf4ae
Juliano Padoin Vieira <proprietyofkurd@hotmail.com>
2021-07-14 11:52:32 -0300

    Commit depois da quarta aula

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 // Usado o hook useState
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function App(props) {
+  const [ usuario, setUsuario ] = useState('');
+
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response));
+
+  }
+
   return (
     // Elementos irmãos só podem ser passados dentro de outra tag, abaixo usou-se o fragment "<>"
+
     <>
       {/* OBS: "class" no javascript é uma palavra reservada, então /usa-se "className" para evitar futuros erros. */}
-      <input name="usuario" id="usuario" className="usuario" placeholder="Usuário" />
-      <button type="button">Pesquisar</button>
+      <input className="usuario" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+      <button type="button" onClick={handlePesquisa} >Pesquisar</button>
     </>
   );
 
